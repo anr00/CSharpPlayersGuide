16aa7044140c915b2e91d798807600ca9e3aac27
anr00 <161641917+anr00@users.noreply.github.com>
2024-03-02 18:21:51 +0000

    Add files via upload

---
+++ 16 - Simula’s Test/Program.cs
@@ -0,0 +1,14 @@
+﻿State current = State.Open;
+
+while (true)
+{
+    Console.WriteLine($"The chest is {current}. What do you want to do?");
+    string answer = Console.ReadLine();
+               
+    if (current == State.Open && answer == "close") current = State.Close;      
+    if (current == State.Close && answer == "open") current = State.Open;
+    if (current == State.Close && answer == "lock") current = State.Lock;
+    if (current == State.Lock && answer == "unlock") current = State.Close;     
+}
+
+enum State { Close, Open, Lock }

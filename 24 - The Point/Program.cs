037d3ad234e7a3ddb7a35200eb65e396916e81fa
anr00 <161641917+anr00@users.noreply.github.com>
2024-04-09 18:52:58 +0000

    Add files via upload

---
+++ 24 - The Point/Program.cs
@@ -0,0 +1,22 @@
+﻿Point origin = new Point(); 
+Console.WriteLine($"{origin.PointX}, {origin.PointY}");
+Point point1 = new Point(2, 3);
+Point point2 = new Point(-4, 0);
+Console.WriteLine($"{point1.PointX}, {point1.PointY}");
+Console.WriteLine($"{point2.PointX}, {point2.PointY}");
+
+public class Point
+{
+	public int PointX { get; }
+	public int PointY { get; }
+	
+	public Point(int pointx, int pointy)
+	{
+		PointX = pointx;
+		PointY = pointy;
+	}
+
+    public Point() : this(0,0) { }
+}
+ 
+ 

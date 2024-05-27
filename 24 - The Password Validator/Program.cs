9c204dedc8390882159cf764a5595653a9c5b3a0
anr00 <161641917+anr00@users.noreply.github.com>
2024-05-27 20:49:35 +0000

    Add files via upload

---
+++ 24 - The Password Validator/Program.cs
@@ -0,0 +1,36 @@
+﻿while (true)
+{
+    Console.WriteLine("Type your password");
+    string user_pass = Console.ReadLine();
+    PasswordValidator password = new PasswordValidator(user_pass);
+    password.Validation();
+}
+
+
+public class PasswordValidator
+{
+    private string Pass;
+    bool Tampersand = false;
+    bool Number;
+    bool Lower;
+    bool Upper;
+
+    public PasswordValidator(string pass)
+    {
+        Pass = pass;
+    }
+
+    public void Validation()
+    {
+        foreach (char l in Pass)
+        {
+            if (l == '&' || l =='T') Tampersand = true;
+            if (char.IsUpper(l)) Upper = true;
+            if (char.IsLower(l)) Lower = true;
+            if (char.IsDigit(l)) Number = true;
+        }
+
+        if (Tampersand == false && Upper == true && Lower == true && Number == true) Console.WriteLine("Everything ok with your pass!");
+        else Console.WriteLine("Check your pass and try again!");
+    }
+}
---
+++ 28 - Room Coordinates/Program.cs
@@ -0,0 +1,19 @@
+﻿public struct Coordinate
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+
+    public Coordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+     public static bool Adjacent(Coordinate a, Coordinate b)
+    {
+        if (Math.Abs(a.Row - b.Row) <= 1 && a.Column - b.Column == 0) return true;
+        if (Math.Abs(a.Column - b.Column) <= 1 && a.Row - b.Row == 0) return true;
+        return false;
+    }
+}
+

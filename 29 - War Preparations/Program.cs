28e4327acd7cd4cf37ca071fe312bee141790278
anr00 <161641917+anr00@users.noreply.github.com>
2024-05-27 21:01:48 +0000

    Add files via upload

---
+++ 29 - War Preparations/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Sword sword = new Sword(Material.Iron, Gemstone.None, 30, 45);
+Sword sword1 = sword with {Material = Material.Wood, Width = 50};
+Sword sword2 = sword with {Gemstone = Gemstone.Amber, Length = 25};
+
+Console.WriteLine(sword);
+Console.WriteLine(sword1);
+Console.WriteLine(sword2);
+
+public record Sword(Material Material, Gemstone Gemstone, float Length, float Width);
+
+public enum Material {Wood, Bronze, Iron, Steel, Binarium}
+
+public enum Gemstone {Emerald, Amber, Sapphire, Diamond, Bitstone, None}
+

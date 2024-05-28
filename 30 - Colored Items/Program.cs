88fb7605e57fdecf69bf584469484e9e4447e457
anr00 <161641917+anr00@users.noreply.github.com>
2024-05-28 12:56:52 +0000

    Add files via upload

---
+++ 30 - Colored Items/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class ColoredItem<T>
+{
+    public T Item { get; }
+    public ConsoleColor Color { get; }
+
+    public ColoredItem(T item, ConsoleColor color)
+    {
+        Item = item;
+        Color = color;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item);
+    }
+}

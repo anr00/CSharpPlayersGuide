4583127979e2acb31acdc9ef343614cff8d21c87
anr00 <161641917+anr00@users.noreply.github.com>
2024-12-30 21:45:09 +0000

    Add files via upload

---
+++ 33 - The Feud/Program.cs
@@ -0,0 +1,20 @@
+﻿using IField;
+using McDroid;
+
+using pig = McDroid.Pig;
+
+Sheep s1 = new Sheep();
+pig p1 = new pig();
+IField.Pig p2 = new IField.Pig();
+
+namespace IField
+{
+    public class Sheep {}
+    public class Pig {}
+}
+
+namespace McDroid
+{
+    public class Cow {}
+    public class Pig {}
+}

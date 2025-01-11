8d76cc5657e596ca1732eec42e23b759191a2c1f
anr00 <161641917+anr00@users.noreply.github.com>
2025-01-11 20:42:02 +0000

    Add files via upload

---
+++ 34 - Safer Number Crunching/Program.cs
@@ -0,0 +1,23 @@
+﻿int n;
+
+while(true)
+{
+    if (int.TryParse(Console.ReadLine(), out n))
+        break;
+}
+
+double n2;
+
+while(true)
+{
+    if (double.TryParse(Console.ReadLine(), out n2))
+        break;
+}
+
+bool boolean;
+
+while(true)
+{
+    if (bool.TryParse(Console.ReadLine(), out boolean))
+        break;
+}

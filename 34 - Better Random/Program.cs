8f12305996f752edfeac4efbe986772520c68275
anr00 <161641917+anr00@users.noreply.github.com>
2025-01-23 20:49:27 +0000

    Add files via upload

---
+++ 34 - Better Random/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class RandomExtensions
+{
+    public static double DoubleRandom(this Random r1)
+    {
+        return r1.NextDouble() * 100;
+    }
+
+    public static string RandomArrow(this Random r1, params string[] arrows)
+    {
+        return arrows[r1.Next(arrows.Length)];
+    }
+    public static bool CoinRandon(this Random r1)
+    {
+        return r1.NextDouble() < r1.NextDouble();
+    }
+}

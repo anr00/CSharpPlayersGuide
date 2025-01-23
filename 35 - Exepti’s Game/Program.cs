8019da91c42338487b0ce8043503cda23819cadc
anr00 <161641917+anr00@users.noreply.github.com>
2025-01-23 21:18:44 +0000

    Add files via upload

---
+++ 35 - Exepti’s Game/Program.cs
@@ -0,0 +1,28 @@
+﻿
+try
+{
+    List<int> numbers = new List<int>();
+    Random r1 = new Random();
+
+    while(true)
+    {
+        int n1;
+        bool tentativa;
+        do
+        {
+            Console.WriteLine("Pick a number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            tentativa = numbers.Contains(n1);
+            if (tentativa)
+                Console.WriteLine("Choose a another number");
+
+        } while (tentativa);
+        if (n1 == r1.Next(0 ,9))
+            throw new Exception();
+        numbers.Add(n1);
+    }
+}
+catch(Exception)
+{
+    Console.WriteLine("You lost!"); 
+}

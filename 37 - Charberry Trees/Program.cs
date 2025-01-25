083c925e60da47f6aeab99f016cd0db538bdbfaf
anr00 <161641917+anr00@users.noreply.github.com>
2025-01-25 19:44:37 +0000

    Add files via upload

---
+++ 37 - Charberry Trees/Program.cs
@@ -0,0 +1,53 @@
+﻿CharberryTree tree = new CharberryTree();
+
+Notifier notifier = new Notifier(tree);
+Harvest harvest = new Harvest(tree);
+
+while (true)
+    tree.MaybeGrow();
+public class CharberryTree
+{
+    private Random _random = new Random();
+    public bool Ripe { get; set; }
+    public event Action? Ripened; 
+
+    public void MaybeGrow()
+    {
+        // Only a tiny chance of ripening each time, but we try a lot!
+        if (_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripened?.Invoke();
+        }
+    }
+}
+
+public class Notifier
+{
+    public Notifier(CharberryTree charberryTree)
+    {
+        charberryTree.Ripened += OnTree;
+    }
+    private void OnTree()
+    {
+        Console.WriteLine("Ready");
+    }
+}
+
+public class Harvest
+{
+    private int count;
+    private CharberryTree charberryTree;
+    public Harvest(CharberryTree _charberryTree)
+    {
+        charberryTree = _charberryTree;
+        charberryTree.Ripened += OnTree;
+    }
+    
+    private void OnTree()
+    {
+        count++;
+        charberryTree.Ripe = false;
+        Console.WriteLine($"It's been harvested {count} times");
+    }
+}

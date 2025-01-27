717f6188043de2185f6bf9f814e0c2d13687b1fd
anr00 <161641917+anr00@users.noreply.github.com>
2025-01-27 21:20:03 +0000

    Add files via upload

---
+++ 39 - The Long Game/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Type your name");
+string? name = Console.ReadLine();
+
+int score = 0;
+
+if (File.Exists(name))
+{
+    score = Convert.ToInt32(File.ReadAllText(name));
+    Console.WriteLine($"You score is: {score}");
+}
+
+while(true)
+{
+    ConsoleKey key = Console.ReadKey().Key;
+    if (key == ConsoleKey.Enter)
+        break;
+    score++;    
+}
+
+File.WriteAllText($"{name}", score.ToString());

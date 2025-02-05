f1576e6123535d132ba0ac2b1fe64227949f2db1
anr00 <161641917+anr00@users.noreply.github.com>
2025-02-05 18:36:16 -0300

    Add files via upload

---
+++ 42 - Query expressions/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] numbers = {1, 9, 2, 8, 3, 7, 4, 6, 5};
+
+foreach (int aaa in LINQ2(numbers))
+    Console.WriteLine(aaa);
+
+IEnumerable<int> Normal(int[] array)
+{
+    List<int> numbers = new List<int>();
+    foreach (int a in array)
+        if (a % 2 == 0)
+            numbers.Add(a);
+
+    int[] ints = numbers.ToArray();
+    Array.Sort(ints);
+
+    for (int i = 0; i < ints.Length; i++)
+        ints[i] *= 2;
+    return ints;
+}
+
+IEnumerable<int> LINQ(int[] array)
+{
+    return from a in array
+           where a % 2 == 0
+           orderby a
+           select a * 2;
+}
+
+IEnumerable<int> LINQ2(int[] array)
+{
+    return array
+           .Where(a => a % 2 == 0)
+           .OrderBy(a => a)
+           .Select(a => a * 2);
+}
---
+++ 43 - Threads/Program.cs
@@ -0,0 +1,56 @@
+﻿RecentNumbers rn = new RecentNumbers();
+Thread thread = new Thread(rn.GenerateNumbers);
+thread.Start();
+
+while(true)
+{
+    Console.ReadKey(false);
+    if (rn.Number1 == rn.Number2)
+        Console.WriteLine("That's a duplicate");
+    else
+        Console.WriteLine("That's not a duplicate");
+}
+
+
+class RecentNumbers
+{
+    private int _number1;
+    private int _number2;
+    private readonly object _numberLock = new object();
+    public int Number1 
+    {
+        get
+        {
+            lock(_numberLock) 
+            {
+                return _number1;
+            }
+        }
+    }
+    public int Number2
+    {
+        get
+        {
+            lock(_numberLock) 
+            {
+                return _number2;
+            }
+        }
+    }
+
+    Random r1  = new Random();
+
+    public void GenerateNumbers()
+    {
+        while(true)
+        {
+            lock(_numberLock)
+            {
+                _number1 = r1.Next(0, 10);
+                _number2 = r1.Next(0, 10);
+                Console.WriteLine($"Number 1: {_number1}, Number 2: {_number2}");
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
